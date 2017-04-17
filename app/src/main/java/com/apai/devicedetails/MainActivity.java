db1c372c4951b7db4c331951a379bfe12a0ddeda
anjanapai2508 <anjanapai2508@gmail.com>
2017-04-16 17:37:54 -0700

    Added code to create excel on drive

--- app/src/main/java/com/apai/devicedetails/MainActivity.java
+++ app/src/main/java/com/apai/devicedetails/MainActivity.java
@@ -1,42 +1,269 @@
 package com.apai.devicedetails;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.IntentSender;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.drive.Drive;
+import com.google.android.gms.drive.DriveApi;
+import com.google.android.gms.drive.DriveContents;
+import com.google.android.gms.drive.DriveFile;
+import com.google.android.gms.drive.DriveFolder;
+import com.google.android.gms.drive.DriveId;
+import com.google.android.gms.drive.MetadataChangeSet;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
     public Button showDetails;
 
-    static final String[] FRUITS = new String[] { "Apple", "Avocado", "Banana",
-            "Blueberry", "Coconut", "Durian"};
-
-    static final String[] animalName={"Lion","Tiger","Monkey","Dog","Cat","Elephant"};
-    static final ArrayList<String>displayStringArrayList = new ArrayList<String>();
+    /* static final String[] FRUITS = new String[] { "Android Build Version", "Screen Resolution", "Device Size",
+             "Device Name", "Model Number", "Added to Inventory on"};*/
+    static final ArrayList<String> deviceHeader = new ArrayList<String>();
+    //static final String[] animalName={"Lion","Tiger","Monkey","Dog","Cat","Elephant"};
+    static final ArrayList<String> details = new ArrayList<String>();
+    static final ArrayList<String> displayStringArrayList = new ArrayList<String>();
     public String displayString = null;
+    private GoogleApiClient mGoogleApiClient;
+    private static final int REQUEST_CODE_RESOLUTION = 1;
+    private boolean fileOperation = false;
+    private DriveId mFileId;
+    public DriveFile file;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView listView = (ListView)findViewById(R.id.detailsList);
-         ArrayList<HashMap<String,String>> arrayList=new ArrayList<>();
-        for (int i=0;i<animalName.length;i++)
-        {
-            displayStringArrayList.add(animalName[i] + " : " + FRUITS[i]);
+        ListView listView = (ListView) findViewById(R.id.detailsList);
+        ArrayList<HashMap<String, String>> arrayList = new ArrayList<>();
+        //String deviceName = android.os.Build.MODEL;
+        String deviceName = getDeviceName();
+        String brand = Build.BRAND;
+        String display = Build.DISPLAY;
+        String product = Build.PRODUCT;
+        System.out.println("brand : " + brand);
+        details.add(deviceName);
+        details.add(brand);
+        details.add(display);
+        details.add(product);
+        deviceHeader.add("Device Name");
+        deviceHeader.add("Brand");
+        deviceHeader.add("Display");
+        deviceHeader.add("Produce");
+
+        for (int i = 0; i < deviceHeader.size(); i++) {
+            displayStringArrayList.add(deviceHeader.get(i) + " : " + details.get(i));
+            System.out.println("display text :  " + deviceHeader.get(i) + " : " + details.get(i));
         }
         ArrayAdapter adapter = new ArrayAdapter<String>(this,
                 R.layout.activity_listview, displayStringArrayList);
-       listView.setAdapter(adapter);
+        listView.setAdapter(adapter);
 
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mGoogleApiClient == null) {
+
+            /**
+             * Create the API client and bind it to an instance variable.
+             * We use this instance as the callback for connection and connection failures.
+             * Since no account name is passed, the user is prompted to choose.
+             */
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addApi(Drive.API)
+                    .addScope(Drive.SCOPE_FILE)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .build();
+        }
+
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient != null) {
+
+            // disconnect Google Android Drive API connection.
+            mGoogleApiClient.disconnect();
+        }
+        super.onPause();
+    }
+
+    public static String getDeviceName() {
+        String manufacturer = Build.MANUFACTURER;
+        String model = Build.MODEL;
+        if (model.startsWith(manufacturer)) {
+            return capitalize(model);
+        }
+        return capitalize(manufacturer) + " " + model;
+    }
+
+    private static String capitalize(String str) {
+        if (TextUtils.isEmpty(str)) {
+            return str;
+        }
+        char[] arr = str.toCharArray();
+        boolean capitalizeNext = true;
+        String phrase = "";
+        for (char c : arr) {
+            if (capitalizeNext && Character.isLetter(c)) {
+                phrase += Character.toUpperCase(c);
+                capitalizeNext = false;
+                continue;
+            } else if (Character.isWhitespace(c)) {
+                capitalizeNext = true;
+            }
+            phrase += c;
+        }
+        return phrase;
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Toast.makeText(getApplicationContext(), "Connected", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i("Main Activity :", "GoogleApiClient connection suspended");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+// Called whenever the API client fails to connect.
+        Log.i("Main Activity :", "GoogleApiClient connection failed: " + connectionResult.toString());
+
+        if (!connectionResult.hasResolution()) {
+
+            // show the localized error dialog.
+            GoogleApiAvailability.getInstance().getErrorDialog(this, connectionResult.getErrorCode(), 0).show();
+            return;
+        }
+        /**
+         *  The failure has a resolution. Resolve it.
+         *  Called typically when the app is not yet authorized, and an  authorization
+         *  dialog is displayed to the user.
+         */
+
+        try {
+
+            connectionResult.startResolutionForResult(this, REQUEST_CODE_RESOLUTION);
+
+        } catch (IntentSender.SendIntentException e) {
+
+            Log.e("Main Activity :", "Exception while starting resolution activity", e);
+        }
+    }
+
+    public void onClickCreateFile(View view){
+        fileOperation = true;
+
+        // create new contents resource
+        Drive.DriveApi.newDriveContents(mGoogleApiClient)
+                .setResultCallback(driveContentsCallback);
+
+    }
+    /**
+     * This is Result result handler of Drive contents.
+     * this callback method call CreateFileOnGoogleDrive() method
+     * and also call OpenFileFromGoogleDrive() method, send intent onActivityResult() method to handle result.
+     */
+    final ResultCallback<DriveApi.DriveContentsResult> driveContentsCallback =
+            new ResultCallback<DriveApi.DriveContentsResult>() {
+                @Override
+                public void onResult(DriveApi.DriveContentsResult result) {
+
+                    if (result.getStatus().isSuccess()) {
+
+                        if (fileOperation == true) {
+
+                            CreateFileOnGoogleDrive(result);
+
+                        }
+                    }
+
+
+                }
+            };
+
+    /**
+     * Create a file in root folder using MetadataChangeSet object.
+     * @param result
+     */
+    public void CreateFileOnGoogleDrive(DriveApi.DriveContentsResult result){
+
+
+        final DriveContents driveContents = result.getDriveContents();
+
+        // Perform I/O off the UI thread.
+        new Thread() {
+            @Override
+            public void run() {
+                // write content to DriveContents
+                OutputStream outputStream = driveContents.getOutputStream();
+                Writer writer = new OutputStreamWriter(outputStream);
+                try {
+                    writer.write("My FIRST DRIVE FILE");
+                    writer.write("Second Line");
+                    writer.close();
+                } catch (IOException e) {
+                    Log.e("Main Activity : ", e.getMessage());
+                }
+
+                MetadataChangeSet changeSet = new MetadataChangeSet.Builder()
+                        .setTitle("GOOGLE DRIVE TEST")
+                        .setMimeType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+                        .setStarred(true).build();
+
+                // create a file in root folder
+                Drive.DriveApi.getRootFolder(mGoogleApiClient)
+                        .createFile(mGoogleApiClient, changeSet, driveContents)
+                        .setResultCallback(fileCallback);
+            }
+        }.start();
+    }
+    /**
+     * Handle result of Created file
+     */
+    final private ResultCallback<DriveFolder.DriveFileResult> fileCallback = new
+            ResultCallback<DriveFolder.DriveFileResult>() {
+                @Override
+                public void onResult(DriveFolder.DriveFileResult result) {
+                    if (result.getStatus().isSuccess()) {
+
+                        Toast.makeText(getApplicationContext(), "file created: "+""+
+                                result.getDriveFile().getDriveId(), Toast.LENGTH_LONG).show();
+
+                    }
+
+                    return;
+
+                }
+            };
 
 }
